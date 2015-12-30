d03d0a60f94865de7e41718c15e03f0f4d9b62c5
Matthew Gaddes <matthew.gaddes@gmail.com>
2015-12-30 00:32:15 +0000

    commit

--- Coffee Companion/Model.swift
+++ Coffee Companion/Model.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit // This must be imported to work with UIImage
 
 // Dictionary to hold coffee types
 var coffees = [Coffee]()
@@ -19,9 +20,9 @@ struct Coffee { // Defines the attributes a coffee must have
     var notes: String
     var quantity: Int
     var podColour: String // Should this be UIColor?
-    //    var icon: UIImage
+    var icon: UIImage
     
-    init (name: String, intensity: Int, size: String, aroma: String, notes: String, quantity: Int, podColour: String) {
+    init (name: String, intensity: Int, size: String, aroma: String, notes: String, quantity: Int, podColour: String, icon: UIImage) {
         self.name = name
         self.intensity = intensity
         self.size = size
@@ -29,6 +30,7 @@ struct Coffee { // Defines the attributes a coffee must have
         self.notes = notes
         self.quantity = quantity
         self.podColour = podColour
+        self.icon = icon
         
         // Populate array with coffee types
         // Each coffee type in this array will have an number associated with it. This number is dependent upon the order in which the coffees are initialised.
@@ -36,13 +38,34 @@ struct Coffee { // Defines the attributes a coffee must have
     }
 }
 
-// Initialise types of coffee
-var cosi = Coffee(name: "Cosi", intensity: 4, size: "Espresso (40ml)", aroma: "Fruity", notes: "Lightly toasted cereal and fruity notes", quantity: 10, podColour: "Brown")
-var roma = Coffee(name: "Roma", intensity: 8, size: "Espresso (40ml)", aroma: "Intense", notes: "Sweet, woody notes and a full, lasting taste", quantity: 10, podColour: "Dark Brown")
+
 
 class Model
 {
-    let array = coffees // Copy array of coffee types
+    private var dictionary = [String:Coffee]() // Dict to hold coffee types. Key = coffee name.
+    var coffeeNames = [String]()
+    var coffeeDetails = [Coffee]()
+    
+    init () {
+        
+        // TODO: quantity for all coffees should be 0 when app first loads
+        
+        // Initialise types of coffee
+        dictionary["Cosi"] = Coffee(name: "Cosi", intensity: 4, size: "Espresso (40ml)", aroma: "Fruity", notes: "Lightly toasted cereal and fruity notes", quantity: 10, podColour: "Brown", icon: UIImage(named:"Cosi.png")!)
+        dictionary["Roma"] = Coffee(name: "Roma", intensity: 8, size: "Espresso (40ml)", aroma: "Intense", notes: "Sweet, woody notes and a full, lasting taste", quantity: 8, podColour: "Dark Brown", icon: UIImage(named:"Roma.png")!)
+        dictionary["Arpeggio"] = Coffee(name: "Arpeggio", intensity: 9, size: "Espresso (40ml)", aroma: "Intense", notes: "Strong character and intense body, enhanced by cocoa notes", quantity: 15, podColour: "Purple", icon: UIImage(named:"Arpeggio.png")!)
+        dictionary["Ristretto"] = Coffee(name: "Ristretto", intensity: 10, size: "Espresso (40ml)", aroma: "Intense", notes: "Full-bodied, intense espresso with a subtle, fruity note", quantity: 13, podColour: "Black", icon: UIImage(named:"Ristretto.png")!)
+        dictionary["Volluto"] = Coffee(name: "Volluto", intensity: 4, size: "Espresso (40ml)", aroma: "Balanced", notes: "Sweet and biscuity flavours", quantity: 7, podColour: "Yellow", icon: UIImage(named:"Volluto.png")!)
+    
+        // Populate 2No. arrays
+        // One will hold the name of each coffee, in order
+        // One will hold all other data, in order
+        // These arrays can then be accessed using subscript syntax
+        for (key, value) in dictionary {
+            coffeeNames.append(key)
+            coffeeDetails.append(value)
+        }
+    }
 }
 
 
--- Coffee Companion/ViewController.swift
+++ Coffee Companion/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var numberOfRemainingPods: UILabel!
     
     let model = Model()
     
+    // TODO: this should update the value within the model itself (not just the label.text)!
     @IBAction func addSleeve(sender: UIButton)
     {
         if let num = Int(numberOfRemainingPods.text!) { // Check label.text can be converted to Int
@@ -23,6 +24,7 @@ class ViewController: UIViewController {
         }
     }
     
+    // TODO: this should update the value within the model itself (not just the label.text)!
     @IBAction func removePod(sender: UIButton)
     {
         if let num = Int(numberOfRemainingPods.text!) { // Check label.text can be converted to Int
@@ -33,13 +35,126 @@ class ViewController: UIViewController {
         }
     }
     
+    // MARK: START - ScrollView
     
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
     
+    var imageView: UIImageView!
+    var pageImages: [UIImage] = []
+    var pageViews: [UIImageView?] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1 Associate each individual image with a page in the Scroll View
+        for value in model.coffeeDetails {
+            pageImages.append(value.icon)
+        }
+        
+        // The numberOfRemainingPods shown when the view first loads should relate to the first image in the array
+        numberOfRemainingPods.text = "\(model.coffeeDetails[0].quantity)"
+        
+        let pageCount = pageImages.count
+        
+        // 2
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = pageCount
+        
+        // 3
+        for _ in 0..<pageCount {
+            pageViews.append(nil)
+        }
+        
+        // 4
+        let pagesScrollViewSize = scrollView.frame.size
+        scrollView.contentSize = CGSizeMake(pagesScrollViewSize.width * CGFloat(pageImages.count), pagesScrollViewSize.height)
+        
+        // 5
+        loadVisiblePages()
     }
+    
+    func loadPage(page: Int) {
+        
+        if page < 0 || page >= pageImages.count {
+            // If it's outside the range of what you have to display, then do nothing
+            return
+        }
+        
+        // 1
+        if let pageView = pageViews[page] {
+            // Do nothing. The view is already loaded.
+        } else {
+            // 2
+            var frame = scrollView.bounds
+            frame.origin.x = frame.size.width * CGFloat(page)
+            frame.origin.y = 0.0
+            
+            // 3
+            let newPageView = UIImageView(image: pageImages[page])
+            newPageView.contentMode = .ScaleAspectFit
+            newPageView.frame = frame
+            scrollView.addSubview(newPageView)
+            
+            // 4
+            pageViews[page] = newPageView
+        }
+    }
+    
+    func purgePage(page: Int) {
+        
+        if page < 0 || page >= pageImages.count {
+            // If it's outside the range of what you have to display, then do nothing
+            return
+        }
+        
+        // Remove a page from the scroll view and reset the container array
+        if let pageView = pageViews[page] {
+            pageView.removeFromSuperview()
+            pageViews[page] = nil
+        }
+    }
+    
+    func loadVisiblePages() {
+        
+        // First, determine which page is currently visible
+        let pageWidth = scrollView.frame.size.width
+        let page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
+        
+        // Update the page control
+        pageControl.currentPage = page
+        
+        // Work out which pages you want to load
+        let firstPage = page - 1
+        let lastPage = page + 1
+        
+        
+        // Purge anything before the first page
+        for var index = 0; index < firstPage; ++index {
+            purgePage(index)
+        }
+        
+        // Load pages in our range
+        for var index = firstPage; index <= lastPage; ++index {
+            loadPage(index)
+        }
+        
+        // Purge anything after the last page
+        for var index = lastPage+1; index < pageImages.count; ++index {
+            purgePage(index)
+        }
+        
+        // TODO: keep numberOfRemainingPods in sync with current coffee
+        numberOfRemainingPods.text = "\(model.coffeeDetails[page].quantity)"
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        loadVisiblePages()
+    }
+    
+    // MARK: END - ScrollView
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
