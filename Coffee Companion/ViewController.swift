dbe556c5760e916504d42f0ab7fe452a207c8a9f
Matthew Gaddes <matthew.gaddes@gmail.com>
2016-02-27 10:37:38 +0000

    Code Cleanup: 1. Delete func loadAllCoffees() 2. Update label within save() func. This eliminates the need to access Core Data twice each time quantity is updated 3. Add/remove comments where appropriate

--- Coffee Companion/ViewController.swift
+++ Coffee Companion/ViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2015 Matthew Gaddes. All rights reserved.
 //
 
+// Tutorials used in this project:
+// UIScrollView - Ray Wenderlich - http://www.raywenderlich.com/76436/use-uiscrollview-scroll-zoom-content-swift
+// Core Data - Ray Wenderlich - http://www.raywenderlich.com/115695/getting-started-with-core-data-tutorial
+// Core Data - Rm2kdev - https://www.youtube.com/watch?v=3IDfgATVqHw
+
 import UIKit
 import CoreData
 
@@ -28,8 +33,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
     @IBAction func addSleeve(sender: UIButton)
     {
         if let _ = Int(quantityRemaining.text!) { // Check that quantityRemaining (String) can be expressed as type Int
-            saveQuantityToDataModel(incrementOrDecrement: 10) // Save newly-updated value to CoreData (increase by 10)
-            loadQuantityFromDataModel() // Load value from CoreData and update label
+            saveQuantityAndUpdateLabel(incrementOrDecrement: 10) // Save newly-updated value to CoreData (increase by 10) and update label
         } else {
             print("Error - Value of quantityRemaining cannot be expressed as type Int")
         }
@@ -39,8 +43,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
     {
         if let num = Int(quantityRemaining.text!) { // Check that quantityRemaining (String) can be expressed as type Int
             if num > 0 { // This prevents number of pods being a negative number
-                saveQuantityToDataModel(incrementOrDecrement: -1) // Save newly-updated value to CoreData (decrease by 1)
-                loadQuantityFromDataModel() // Load value from CoreData and update label
+                saveQuantityAndUpdateLabel(incrementOrDecrement: -1) // Save newly-updated value to CoreData (decrease by 1) and update label
             }
         } else {
             print("Error - Value of quantityRemaining cannot be expressed as type Int")
@@ -51,7 +54,6 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var pageControl: UIPageControl!
     
-//    var imageView: UIImageView!
     var pageImages: [UIImage] = []
     var pageViews: [UIImageView?] = []
     
@@ -68,18 +70,16 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
     }
     
     func showImagesForCurrentPickerCategory() {
+        
         // Clear array of page images so that new images can be loaded
         resetValuesAndArrays()
+        
+        // Comments 1-5: Ray Wenderlich - UIScrollView Tutorial - http://goo.gl/FflMhj - Ctrl-F "Next, implement viewDidLoad as follows"
         
         // 1 Associate each individual image with a page in the Scroll View
         for value in currentPickerCategory {
             pageImages.append(value.icon)
         }
-        
-        // The quantityRemaining shown when the view first loads should relate to the current page.
-        // MARK: currentPage should stay the same when user closes and re-opens the app.
-//        quantityRemaining.text = "\(currentPickerCategory[currentPage].quantity)" // TODO: update to work with Core Data - call function updateQuantityRemainingLabel()
-//        updateQuantityRemainingLabel() - THIS function is already called from within loadVisiblePages()
         
         let pageCount = pageImages.count
         
@@ -113,6 +113,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
             // If it's outside the range of what you have to display, then do nothing
             return
         }
+        
+        // Comments 1-4: Ray Wenderlich - UIScrollView Tutorial - http://goo.gl/FflMhj - Ctrl-F "Remember each page is a UIImageView stored in an array of optionals"
         
         // 1
         if let _ = pageViews[page] {
@@ -183,12 +185,121 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
         updateInfoLabels()
     }
     
+    func updateInfoLabels() {
+        nameLabel.text = "Name: \(currentPickerCategory[currentPage].name)"
+//        intensityLabel.text = "Intensity: \(model.coffeeDetails[currentPage].intensity)"
+//        sizeLabel.text = "Size: \(model.coffeeDetails[currentPage].size)"
+//        aromaLabel.text = "Aroma: \(model.coffeeDetails[currentPage].aroma)"
+//        notesLabel.text = "Notes: \(model.coffeeDetails[currentPage].notes)"
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        loadVisiblePages()
+    }
+    
+    // MARK: - PickerView
+    var pickerData = ["Intenso", "Espresso", "Pure Origin", "Lungo", "Decaffeinato", "Variations"]
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let string = pickerData[row]
+        return NSAttributedString(string: string, attributes: [NSForegroundColorAttributeName:UIColor.whiteColor()])
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        switch row {
+        case 0:
+            print("Intenso!")
+            currentPickerCategory = model.intensoArray // Update currentPickerCategory
+            showImagesForCurrentPickerCategory() // Load images from intensoArray
+        case 1:
+            print("Espresso!")
+            currentPickerCategory = model.espressoArray
+            showImagesForCurrentPickerCategory()
+        case 2:
+            print("Pure Origin!")
+            currentPickerCategory = model.pureOriginArray
+            showImagesForCurrentPickerCategory()
+        case 3:
+            print("Lungo!")
+            currentPickerCategory = model.lungoArray
+            showImagesForCurrentPickerCategory()
+        case 4:
+            print("Decaffeinato!")
+            currentPickerCategory = model.decaffeinatoArray
+            showImagesForCurrentPickerCategory()
+        case 5:
+            print("Variations!")
+            currentPickerCategory = model.variationsArray
+            showImagesForCurrentPickerCategory()
+        default:
+            break
+        }
+    }
+    
+    // MARK: - CoreData
+    func saveQuantityAndUpdateLabel(incrementOrDecrement incrementOrDecrement: Int) { // Save to CoreData
+        
+        let coffeeName = currentPickerCategory[currentPage].name
+        print("SAVE - current coffee name = \(coffeeName)")
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        // Fetch individual coffee type from CoreData using predicates
+        let request = NSFetchRequest(entityName: "Coffee") // Ask database to perform a request on the "Coffee" table
+        request.returnsObjectsAsFaults = false // Prevent CoreData from returning objects as faults
+        request.predicate = NSPredicate(format: "name = %@", coffeeName) // Create predicate - only fetch the coffee we wish to update
+        
+        do { // Execute fetch request in a safe way
+            
+            let resultArray = try managedContext.executeFetchRequest(request) // resultArray is used because it is safer to accept results into an array (rather than a single AnyObject), just in case more than one result is returned. This may happen in the case where two coffees with the same name have been erroneously saved to CoreData.
+            
+            if resultArray.count == 1 { // We only expect a single result
+                
+                let result = resultArray[0] // Assign first (and only) result to constant named result.
+                
+                // Update quantity and save to Core Data, all in one step
+                if let currentQuantity = result.valueForKey("quantity") as! Int? { // Downcast quantity to type Int
+                    let updatedQuantity = currentQuantity + incrementOrDecrement // Calculate updated quantity by adding or subtracting (as appropriate)
+                    result.setValue(updatedQuantity, forKey: "quantity") // Assign updated quantity to selected coffee in Core Data
+                    quantityRemaining.text = "\(updatedQuantity)" // Update label
+                } else {
+                    print("Error - 'quantity' is not an Int")
+                }
+                
+                // Save quantity of selected coffee only
+                do {
+                    try managedContext.save()
+                    print("Saved successfully!")
+                } catch let error as NSError  {
+                    print("Error - Could not save \(error), \(error.userInfo)")
+                }
+            } else {
+                print("Potential Error - \(resultArray.count) results returned.")
+            }
+        } catch let error as NSError {
+            print("Error - Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
     func loadQuantityFromDataModel() {
         
         // For the currently-selected coffee type, this function loads the value of "quantity" from Core Data and uses it to update the label on screen (quantityRemaining)
         
         let coffeeName = currentPickerCategory[currentPage].name
-        print("UPDATE - current coffee name = \(coffeeName)")
+        print("LOAD - current coffee name = \(coffeeName)")
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
@@ -217,159 +328,14 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIPickerViewDelega
         }
     }
     
-    func updateInfoLabels() {
-        nameLabel.text = "Name: \(currentPickerCategory[currentPage].name)"
-//        intensityLabel.text = "Intensity: \(model.coffeeDetails[currentPage].intensity)"
-//        sizeLabel.text = "Size: \(model.coffeeDetails[currentPage].size)"
-//        aromaLabel.text = "Aroma: \(model.coffeeDetails[currentPage].aroma)"
-//        notesLabel.text = "Notes: \(model.coffeeDetails[currentPage].notes)"
-    }
-    
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        loadVisiblePages()
-    }
-
-    // MARK: - PickerView
-    var pickerData = ["Intenso", "Espresso", "Pure Origin", "Lungo", "Decaffeinato", "Variations"]
-    
-    // The number of columns of data
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // The number of rows of data
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-    
-    // The data to return for the row and component (column) that's being passed in
-    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-        let string = pickerData[row]
-        return NSAttributedString(string: string, attributes: [NSForegroundColorAttributeName:UIColor.whiteColor()])
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-        switch row {
-        case 0:
-            print("Intenso!")
-            currentPickerCategory = model.intensoArray // 1. Update currentPickerCategory
-            showImagesForCurrentPickerCategory() // 2. Load images from intensoArray
-        case 1:
-            print("Espresso!")
-            currentPickerCategory = model.espressoArray
-            showImagesForCurrentPickerCategory()
-        case 2:
-            print("Pure Origin!")
-            currentPickerCategory = model.pureOriginArray
-            showImagesForCurrentPickerCategory()
-        case 3:
-            print("Lungo!")
-            currentPickerCategory = model.lungoArray
-            showImagesForCurrentPickerCategory()
-        case 4:
-            print("Decaffeinato!")
-            currentPickerCategory = model.decaffeinatoArray
-            showImagesForCurrentPickerCategory()
-        case 5:
-            print("Variations!")
-            currentPickerCategory = model.variationsArray
-            showImagesForCurrentPickerCategory()
-        default:
-            break
-        }
+    // MARK: - Default Overrides
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: - CoreData
-    // TODO: Write function to retrieve (and return) the currently selected coffee? This would avoid repetition in the save() and update() functions, for example.
-    
-    func saveQuantityToDataModel(incrementOrDecrement incrementOrDecrement: Int) { // Save to CoreData
-        
-        // Useful Links:
-        // http://www.raywenderlich.com/115695/getting-started-with-core-data-tutorial
-        // https://www.youtube.com/watch?v=3IDfgATVqHw
-        
-        // 1 - Fetch individual coffee type from CoreData (use predicates?)
-        // 2 - Update quantity of coffee
-        // 3 - Save coffee back to CoreData
-        let coffeeName = currentPickerCategory[currentPage].name
-        print("SAVE - current coffee name = \(coffeeName)")
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        
-        // 1 - Fetch individual coffee type from CoreData (use predicates?)
-        let request = NSFetchRequest(entityName: "Coffee") // Ask database to perform a request on the "Coffee" table
-        request.returnsObjectsAsFaults = false // Prevent CoreData from returning objects as faults
-        request.predicate = NSPredicate(format: "name = %@", coffeeName) // Create predicate - only fetch the coffee we wish to update
-        
-        do { // Execute fetch request in a safe way
-            
-            let resultArray = try managedContext.executeFetchRequest(request) // resultArray is used because it is safer to accept results into an array (rather than a single AnyObject), just in case more than one result is returned. This may happen in the case where two coffees with the same name have been erroneously saved to CoreData.
-            
-            if resultArray.count == 1 { // We only expect a single result
-                
-                let result = resultArray[0] // Assign first (and only) result to constant named result.
-                
-                // Update quantity and save to Core Data, all in one step
-                if let currentQuantity = result.valueForKey("quantity") as! Int? { // Downcast quantity to type Int
-                    let updatedQuantity = currentQuantity + incrementOrDecrement // Calculate updated quantity by adding or subtracting (as appropriate)
-                    result.setValue(updatedQuantity, forKey: "quantity") // Assign updated quantity to selected coffee in Core Data
-                } else {
-                    print("Error - 'quantity' is not an Int")
-                }
-                
-                // Save quantity of selected coffee only
-                do {
-                    try managedContext.save()
-                    print("Saved successfully!")
-                } catch let error as NSError  {
-                    print("Error - Could not save \(error), \(error.userInfo)")
-                }
-            } else {
-                print("Potential Error - \(resultArray.count) results returned.")
-            }
-        } catch let error as NSError {
-            print("Error - Could not fetch \(error), \(error.userInfo)")
-        }
-    }
-    
-    func loadAllCoffees() { // Load from CoreData
-//        let currentCoffee = currentPickerCategory[currentPage]
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        
-        let request = NSFetchRequest(entityName: "Coffee") // Ask database to perform a request on the "Coffee" table
-        request.returnsObjectsAsFaults = false // Prevent CoreData from returning objects as faults
-        
-        do { // Execute fetch request in a safe way
-            let results: NSArray = try managedContext.executeFetchRequest(request)
-            
-            if results.count > 0 { // very basic error handling
-                for res in results {
-                    print(res)
-                }
-            } else {
-                print("Potential Error - \(results.count) results returned")
-            }
-        } catch let error as NSError {
-            print("Could not fetch \(error), \(error.userInfo)")
-        }
-    }
-    
-    // Re-write load() function using predicates
-    // function should be called each time user swipes/changes coffee type
-    // this way, the number (coffeeQuantity) shown to the user will always be the most up to date i.e. whatever was last saved to CoreData
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        loadAllCoffees()
     }
 }
 
